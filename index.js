6e4b482e001c720209f1b5b0cb66594f1609f8e7
Muatasim Qazi <muatasimqazi@gmail.com>
2017-11-22 23:36:21 -0800

    fixed typo with challenge

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function(req, res) {
 
 app.get('/webhook/', function(req, res) {
   if (req.query['hub.verify_token'] === 'balochiwords') {
-    res.send(req.query['hub.challeng'])
+    res.send(req.query['hub.challenge'])
   }
   res.send("Wrong token")
 })

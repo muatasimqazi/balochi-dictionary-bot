ccc6860785f0a6e87890eaed20715637c0347160
Muatasim Qazi <muatasimqazi@gmail.com>
2017-11-23 03:30:16 -0800

    added search module

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 
-var person = require('search');
+var person = require('./search');
 
 
 

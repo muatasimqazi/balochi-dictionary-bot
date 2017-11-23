a6067569a14961ab4c7cafdeb7769f3e20a2af97
Muatasim Qazi <muatasimqazi@gmail.com>
2017-11-22 23:29:23 -0800

    fixed typo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const request = require('request')
 
 const app = express()
 
-app.set('port', (process.env.PORT || '5000'))
+app.set('port', (process.env.PORT || 5000))
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())

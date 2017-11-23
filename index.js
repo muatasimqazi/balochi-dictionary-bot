021e06ca3902e0ea4dcd83f1a5ae571aa81a401b
Muatasim Qazi <muatasimqazi@gmail.com>
2017-11-23 07:45:21 -0800

    fixed requested word issue

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/webhook/', function(req, res) {
     let sender = event.sender.id
     if(event.message && event.message.text ) {
       let text = event.message.text
-      qu = "select B, D, E where C = '" + text + "'"
+      let qu = "select B, D, E where C = '" + text + "'"
       console.log("IN");
       getData(qu);
       // setText(text);

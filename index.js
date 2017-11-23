678bec59a44c3d8329fa75c88e092cf89b6472f6
Muatasim Qazi <muatasimqazi@gmail.com>
2017-11-22 23:30:36 -0800

    fixed typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const express = require('express')
-const bodyParser = require('body-parsr')
+const bodyParser = require('body-parser')
 const request = require('request')
 
 const app = express()

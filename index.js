11949aa4aff0ebaed323e71706d7ea7498f59fdb
Muatasim Qazi <muatasimqazi@gmail.com>
2017-11-23 05:34:19 -0800

    added google api

--- index.js
+++ index.js
@@ -23,52 +23,9 @@ app.get('/', function(req, res) {
   res.send('Hi I am a chatbot')
 })
 
-let token = "EAAFfncDwieMBAIzNr8GWU5pBKsOv1tDyUm8mZABiUoF2qOfWrutd6TmYud3bhokT5CpEFXq9bkMnABeBpzbwXZCLrcM2fVMuKjHvJ665BhiWjw7gSzGBj1TTWK0QgLAzZAAAgA792xrwrKa1kkZCCrwRZAito20LsOGa483hGHwZDZD"
 
 
-app.get('/webhook/', function(req, res) {
-  if (req.query['hub.verify_token'] === 'balochiwords') {
-    res.send(req.query['hub.challenge'])
-  }
-  res.send("Wrong token")
-})
-
-
-app.post('/webhook/', function(req, res) {
-  let messaging_events = req.body.entry[0].messaging
-  for(let i = 0; i < messaging_events.length; i++) {
-    let event = messaging_events[i]
-    let sender = event.sender.id
-    if(event.message && event.message.text ) {
-      let text = event.message.text
-      sendText(sender, "Text echo : " + text.substring(0, 100) + ' ' + word.title + ' ' + word.definition)
-    }
-  }
-  res.sendStatus(200)
-})
-
-
-
-// console.log(person1.fullName());
-
-function sendText(sender, text) {
-  let messageData = {text: text}
-  request({
-    url: "https://graph.facebook.com/v2.6/me/messages",
-    qs: {access_token: token},
-    method: "POST",
-    json: {
-      recipient: {id: sender},
-      message: messageData
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log("sending error");
-    } else if (response.body.error) {
-      console.log("response body error");
-    }
-  })
-}
+// google
 
 
 // If modifying these scopes, delete your previously saved credentials
@@ -201,6 +158,55 @@ function listMajors(auth) {
     }
   }
   });
+}
+
+// google end
+
+let token = "EAAFfncDwieMBAIzNr8GWU5pBKsOv1tDyUm8mZABiUoF2qOfWrutd6TmYud3bhokT5CpEFXq9bkMnABeBpzbwXZCLrcM2fVMuKjHvJ665BhiWjw7gSzGBj1TTWK0QgLAzZAAAgA792xrwrKa1kkZCCrwRZAito20LsOGa483hGHwZDZD"
+
+
+app.get('/webhook/', function(req, res) {
+  if (req.query['hub.verify_token'] === 'balochiwords') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send("Wrong token")
+})
+
+
+app.post('/webhook/', function(req, res) {
+  let messaging_events = req.body.entry[0].messaging
+  for(let i = 0; i < messaging_events.length; i++) {
+    let event = messaging_events[i]
+    let sender = event.sender.id
+    if(event.message && event.message.text ) {
+      let text = event.message.text
+      sendText(sender, "Text echo : " + text.substring(0, 100) + ' ' + word.title + ' ' + word.definition)
+    }
+  }
+  res.sendStatus(200)
+})
+
+
+
+// console.log(person1.fullName());
+
+function sendText(sender, text) {
+  let messageData = {text: text}
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: token},
+    method: "POST",
+    json: {
+      recipient: {id: sender},
+      message: messageData
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log("sending error");
+    } else if (response.body.error) {
+      console.log("response body error");
+    }
+  })
 }
 
 
